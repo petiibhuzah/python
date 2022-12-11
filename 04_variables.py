06cb266a32bdbdc2ff19bcd56bd7ffa52cd08097
Peter Daniel Kilimba <petiibhuzah@gmail.com>
2022-12-11 11:10:17 +0300

    Variables

--- 04_variables.py
+++ 04_variables.py
@@ -13,3 +13,19 @@ print(f' X : {x}, Y : {y}, Z : {z}')
 # Get The Type of The Variable
 # Types in Python are classes
 print(f' X : {type(x)}, Y : {type(y)}, Z : {type(z)}')
+
+"""
+VARIABLE NAMES
+1. A variable name must start with a letter or the underscore character
+2. A variable name cannot start with a number
+3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+"""
+
+username = "Peter Daniel"
+userName = "Peter Daniel"  # ✅ My Favourite
+user_name = "Peter Daniel"
+_userName = "Peter Daniel"
+_user_name = "Peter Daniel"
+USERNAME = "Peter Daniel"
+userName1 = "Peter Daniel"
