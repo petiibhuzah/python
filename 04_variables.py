0e3b65fb275a9014569a67d3fb7e19a28407baaf
Peter Daniel Kilimba <petiibhuzah@gmail.com>
2022-12-11 11:01:44 +0300

    Variables

---
+++ 04_variables.py
@@ -0,0 +1,15 @@
+# Declaration of Variable
+x = 4
+x = "Ibhuzah"
+print(f' The Value of X is {x}')
+
+# Variable Casting
+x = str(5)
+y = int('5')
+z = float(7)
+
+print(f' X : {x}, Y : {y}, Z : {z}')
+
+# Get The Type of The Variable
+# Types in Python are classes
+print(f' X : {type(x)}, Y : {type(y)}, Z : {type(z)}')
