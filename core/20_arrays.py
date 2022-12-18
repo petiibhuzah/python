c65e6a595755c01e3df285ba693f8467d8971022
Peter Daniel Kilimba <petiibhuzah@gmail.com>
2022-12-18 14:05:13 +0300

    Arrays

---
+++ core/20_arrays.py
@@ -0,0 +1,14 @@
+"""
+Arrays
+"""
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+# Access the Elements
+x = cars[0]
+print(x)
+# Modify the value of the first array item:
+cars[0] = "Toyota"
+print(cars)
+# The Length of an Array
+x = len(cars)
+print(x)
